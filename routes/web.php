6c6597d1d1d317a92bd9c8d6d8d25d27fbd6fddb
dhani <rahmatdhaniii38@gmail.com>
2026-07-02 01:42:52 +0800

    feat: menyelesaikan desain navbar, footer, halaman beranda, jadwaltravel, detail, login & register

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/beranda', function () {
+    return view('beranda');
+});
+
+Route::get('/jadwal', function () {
+    return view('jadwaltravel');
+});
+
+Route::get('/detail-travel', function () {
+    return view('detailtravel');
+});
+
+// Rute Halaman Login
+Route::get('/login', function () {
+    return view('login');
+});
+
+// Rute Halaman Register
+Route::get('/register', function () {
+    return view('register');
+});
