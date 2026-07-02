95dc13b76efc47da26c62170ebb5177e35bf3d05
venusskip <venusskip@gmail.com>
2026-07-03 01:27:03 +0800

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password', 'role', 'phone', 'address'])] // Tambahkan role, phone, address di sini
+
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
